988e4791af9faa94291a8927f7d0bad218eb649a
Wasim Adil <121689324+wasimadildev@users.noreply.github.com>
2024-04-25 12:39:08 -0700

    connect to  mongo altas

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () =>{
+    try {
+       const connectionINstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+
+       console.log(`MongoDB connected !! DB HOST ${
+        connectionINstance.connection.host
+       }`)
+    } catch (error) {
+        console.log("MONGODB connection error", error)
+        process.exit(1)
+    }
+}
+
+
+export default connectDB
